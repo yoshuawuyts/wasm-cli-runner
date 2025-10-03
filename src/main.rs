71e2e69862dc529abea8222a661aed0db9b4004b
Yosh <github@yosh.is>
2025-10-04 00:15:11 +0200

    add more files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Command;
+
 fn main() {
-    println!("hello");
+    let commands = vec![make_command("bar"), make_command("foo")];
+    let mut cli = Command::new("test")
+        .subcommand_required(true)
+        .arg_required_else_help(true);
+
+    for cmd in commands {
+        cli = cli.subcommand(cmd);
+    }
+
+    // Augment with derived subcommands
+    let matches = cli.get_matches();
+}
+
+fn make_command(name: &'static str) -> Command {
+    Command::new(&name).about(&format!("the {name} command"))
 }
