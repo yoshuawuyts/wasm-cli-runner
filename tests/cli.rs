8873db0c548527f3b862c96d402c104942f009c6
Yosh <github@yosh.is>
2025-10-06 01:52:31 +0200

    more tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -10,29 +10,21 @@ fn has_a_version() {
     assert_cmd_snapshot!(cli().arg("--version"));
 }
 
-// #[test]
-// fn path_is_required() {
-//     let mut cmd = Command::cargo_bin("wasm-cli-runner").unwrap();
-//     let assert = cmd.assert();
-//     let msg = "the following required arguments were not provided:";
-//     assert.failure().stderr(predicates::str::contains(msg));
-// }
-//
-// #[test]
-// fn path_is_reachable_on_disk() {
-//     let mut cmd = Command::cargo_bin("wasm-cli-runner").unwrap();
-//     let assert = cmd.arg("noop.wasm").assert();
-//     let msg = "No such file or directory";
-//     assert.failure().stderr(predicates::str::contains(msg));
-// }
-//
-// #[test]
-// fn path_is_a_wasm_binary() {
-//     let mut cmd = Command::cargo_bin("wasm-cli-runner").unwrap();
-//     let assert = cmd.write_stdin("noop.wasm").assert();
-//     assert.failure().stderr("Could not find file at location\n");
-// }
-//
+#[test]
+fn path_is_required() {
+    assert_cmd_snapshot!(cli());
+}
+
+#[test]
+fn path_is_reachable_on_disk() {
+    assert_cmd_snapshot!(cli().arg("noop.wasm"));
+}
+
+#[test]
+fn path_ends_with_wasm() {
+    assert_cmd_snapshot!(cli().arg("tests/binaries/time.wasm"));
+}
+
 // #[test]
 // fn path_is_a_wasm_component() {
 //     let mut cmd = Command::cargo_bin("wasm-cli-runner").unwrap();
